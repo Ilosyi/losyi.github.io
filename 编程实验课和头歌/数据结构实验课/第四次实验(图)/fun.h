0abb2f530e28f3c699f48a36ef17858468798220
Ilosyi <142215557+Ilosyi@users.noreply.github.com>
2024-05-29 00:03:19 +0900

    Update fun.h

--- 编程实验课和头歌/数据结构实验课/第四次实验(图)/fun.h
+++ 编程实验课和头歌/数据结构实验课/第四次实验(图)/fun.h
@@ -202,3 +202,270 @@ status InsertVex(ALGraph& G, VertexType v)
 
     return OK;
 }
+status DeleteVex(ALGraph& G, KeyType v) {
+    int i, j;
+    ArcNode* p, * q;
+    
+    // 定位要删除的结点
+    j = LocateVex(G, v);
+    if (j == -1) return ERROR;//如果没找到，返回ERROR
+
+    // 删除目标结点所有关联的边
+    p = G.vertices[j].firstarc;
+    while (p) {
+        q = p;
+        p = p->nextarc;
+        free(q);
+        G.arcnum--;
+    }
+
+    //顶点数目减一
+    G.vexnum--;
+
+    // 移动顶点位置
+    for (i = j; i < G.vexnum; i++) {
+        G.vertices[i] = G.vertices[i + 1];
+    }
+    q = NULL;
+    // 更新剩余顶点的边
+    for (i = 0; i < G.vexnum; i++) {
+        p = G.vertices[i].firstarc;
+        while (p) {
+            if (p->adjvex == j) { //被删除的顶点
+                if (p == G.vertices[i].firstarc) //如果此时p为i关联的第一条边
+                {
+                    G.vertices[i].firstarc = p->nextarc;//将i的第一条关联边更新
+                    free(p);//释放该边
+                    p = G.vertices[i].firstarc;
+                }
+                else {
+                    if (q) {
+                        q->nextarc = p->nextarc;
+                        free(p);//释放该边
+                        p = q->nextarc;
+                    }  
+                }
+            }
+            else {
+                //如果关联的是j后面的顶点
+                if (p->adjvex > j)
+                    p->adjvex--;//将其对应的数值减1（因为在数组中向前移动了
+                q = p;//q为i上一条关联的边
+                p = p->nextarc;//下一条i关联边
+            }
+        }
+    }
+
+    return OK;
+}
+status InsertArc(ALGraph& G, KeyType v, KeyType w)
+//在图G中增加弧<v,w>，成功返回OK,否则返回ERROR
+{
+    int i, j;
+    i = LocateVex(G, v);
+    j= LocateVex(G, w);
+    if (i == -1 || j == -1) return ERROR;
+    ArcNode* p = G.vertices[i].firstarc;
+    while (p)
+    {
+        if (p->adjvex == j) return ERROR;
+        else p = p->nextarc;
+    }
+    ArcNode* q = (ArcNode*)malloc(sizeof(ArcNode));
+    q->adjvex = j;
+    q->nextarc = G.vertices[i].firstarc;
+    G.vertices[i].firstarc = q;
+
+    ArcNode *r = (ArcNode*)malloc(sizeof(ArcNode));
+    r->adjvex = i;
+    r->nextarc = G.vertices[j].firstarc;
+    G.vertices[j].firstarc = r;
+    G.arcnum ++;
+    return OK;
+}
+status DeleteArc(ALGraph& G, KeyType v, KeyType w)
+//在图G中删除弧<v,w>，成功返回OK,否则返回ERROR
+{
+    int i, j;
+    int flag1=0,flag2=0;
+    i = LocateVex(G, v);
+    j = LocateVex(G, w);
+    if (i == -1 || j == -1) return ERROR;
+    ArcNode* p = G.vertices[i].firstarc;
+    ArcNode* q = NULL;
+    while (p&&!flag1)
+    {
+        if (p->adjvex == j)
+        {
+            if (p == G.vertices[i].firstarc) //如果此时p为i关联的第一条边
+            {
+                G.vertices[i].firstarc = p->nextarc;//将i的第一条关联边更新
+                free(p);//释放该边
+                p = G.vertices[i].firstarc;
+                
+            }
+            else {
+                if (q) {
+                    q->nextarc = p->nextarc;
+                    free(p);//释放该边
+                    p = q->nextarc;
+                }
+            }
+            flag1 = 1;
+        }
+        else {
+            //如果不是，往后遍历
+            q = p;//q为i上一条关联的边
+            p = p->nextarc;//下一条i关联边
+        }
+    } 
+    p = G.vertices[j].firstarc;
+    q = NULL;
+    while (p&&!flag2)
+    {
+        if (p->adjvex == i)
+        {
+            if (p == G.vertices[j].firstarc) //如果此时p为i关联的第一条边
+            {
+                G.vertices[j].firstarc = p->nextarc;//将i的第一条关联边更新
+                free(p);//释放该边
+                p = G.vertices[i].firstarc;
+            }
+            else {
+                if (q) {
+                    q->nextarc = p->nextarc;
+                    free(p);//释放该边
+                    p = q->nextarc;
+                }
+            }
+            flag2 = 1;
+        }
+        else {
+            //如果不是，往后遍历
+            q = p;//q为i上一条关联的边
+            p = p->nextarc;//下一条i关联边
+        }
+    }
+    if (flag1 && flag2) {
+        G.arcnum--;
+        return OK;
+    }
+    else return ERROR;
+}
+void visit(VertexType v)
+{
+    printf(" %d %s", v.key, v.others);
+}
+void DFS(ALGraph& G, int v, bool visited[]) {
+    // 标记为已访问
+    visited[v] = true;
+    visit(G.vertices[v].data);
+
+
+    ArcNode* p = G.vertices[v].firstarc;
+    while (p) {
+        if (!visited[p->adjvex]) {
+            DFS(G, p->adjvex, visited);
+        }
+        p = p->nextarc;
+    }
+}
+status DFSTraverse(ALGraph& G)
+//对图G进行深度优先搜索遍历，依次对图中的每一个顶点使用函数visit访问一次，且仅访问一次
+{
+
+    bool Visited[MAX_VERTEX_NUM] = { 0 };
+    for (int i = 0; i < G.vexnum; i++) {
+        if (!Visited[i]) {
+            DFS(G, i, Visited);
+        }
+    }
+    return OK;
+}
+void BFSTraverse(ALGraph& G) {
+    bool visited[MAX_VERTEX_NUM] = { 0 };
+    queue<int> Q;
+
+    for (int i = 0; i < G.vexnum; ++i) {
+        if (!visited[i])//如果未访问过
+        {
+            visited[i] = true;
+            visit(G.vertices[i].data);
+            Q.push(i);  //入队
+
+            while (!Q.empty())//当队列不为空
+            {
+                int v = Q.front();//已访问的出队，找它的下一层
+                Q.pop();
+                for (ArcNode* p = G.vertices[v].firstarc; p != NULL; p = p->nextarc) {
+                    int w = p->adjvex;
+                    if (!visited[w]) //将下一层访问并入队
+                    {
+                        visited[w] = true;
+                        visit(G.vertices[w].data);
+                        Q.push(w);
+                    }
+                }
+            }
+        }
+    }
+}
+void PrintfGraph(ALGraph G)
+{
+    for (int j = 0; j < G.vexnum; j++)
+    {
+        ArcNode* p = G.vertices[j].firstarc;
+        printf("%d %s", G.vertices[j].data.key, G.vertices[j].data.others);
+        while (p)
+        {
+            printf(" %d", p->adjvex);
+            p = p->nextarc;
+        }
+        printf("\n");
+    }
+}
+status SaveGraph(ALGraph G, char FileName[])
+//将图的数据写入到文件FileName中
+{
+    FILE* fp = fopen(FileName, "w");
+    for (int i = 0; i < G.vexnum; i++)
+    {
+        fprintf(fp, "%d %s ", G.vertices[i].data.key, G.vertices[i].data.others);
+    }
+    fprintf(fp, "-1 nill ");
+    for (int i = 0; i < G.vexnum; i++)
+    {
+        ArcNode* p = G.vertices[i].firstarc;
+        int VR[101][2];
+        while (p)
+        {
+            if (p->adjvex > i)
+            {
+                fprintf(fp, "%d %d ", G.vertices[i].data.key, G.vertices[p->adjvex].data.key);
+            }
+            p = p->nextarc;
+        }
+    }
+    fprintf(fp, "-1 -1");
+    fclose(fp);
+    return OK;
+}
+status LoadGraph(ALGraph& G, char FileName[])
+//读入文件FileName的图数据，创建图的邻接表
+{
+    VertexType V[21];
+    KeyType VR[100][2];
+    FILE* fp = fopen(FileName, "r");
+    int i = 0;
+    //从文件中读入数据到数组
+    do {
+        fscanf(fp,"%d%s", &V[i].key, V[i].others);
+    } while (V[i++].key != -1);
+    i = 0;
+    do {
+        fscanf(fp,"%d%d", &VR[i][0], &VR[i][1]);
+    } while (VR[i++][0] != -1);
+    CreateCraph(G, V, VR);//调用Creat函数
+    fclose(fp);
+    return OK;
+}
