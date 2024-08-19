80251b2a6dfc9cc010b3776b1b7d9f41b946df98
Ilosyi <142215557+Ilosyi@users.noreply.github.com>
2024-08-19 16:12:33 +0800

    Add files via upload

---
+++ C++练习/QT网络编程基础:TCP/TCPclient/widget.h
@@ -0,0 +1,32 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTcpSocket>
+#include<QHostAddress>//ip地址
+#include<QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_canceButton_clicked();
+
+    void on_okButton_clicked();
+
+private:
+    Ui::Widget *ui;
+    QTcpSocket *socket;
+};
+#endif // WIDGET_H
